cd0eee39f774600ecf3dacbb3bcc68e1b350466e
Manas-Kishore <95855211+Manas-kishore@users.noreply.github.com>
2022-07-20 05:19:11 -0700

    Update ai.py

--- ai.py
+++ ai.py
@@ -4,8 +4,6 @@ import datetime
 import wikipedia 
 import webbrowser
 import os
-import smtplib
-import pyautogui
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -28,7 +26,7 @@ def wishMe():
     else:
         speak("Good Evening!")  
 
-    speak("I am KLAW. Please tell me how may I help you")       
+    speak("I am claw. Please tell me how may I help you")       
 
 def takeCommand():
 
@@ -49,14 +47,6 @@ def takeCommand():
         return "None"
     return query
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('yourmail', 'your pass')
-    server.sendmail('your mail', to, content)
-    server.close()
-
 if __name__ == "__main__":
     wishMe()
     while True:
@@ -70,37 +60,13 @@ if __name__ == "__main__":
             print(results)
             speak(results)
 
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
-
         elif 'open google' in query:
             webbrowser.open("google.com")
-
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")   
-            
-        elif 'play music' in query:
-            music_dir = ''
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
 
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             print(f"Sir, the time is {strTime}")
             speak(f"Sir, the time is {strTime}")
-
-
-        elif 'email to bill' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "xyzemail@gmail.com"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry my friend. I am not able to send this email") 
 
         elif 'shut down' in query:
             speak('shutting down')
@@ -114,16 +80,9 @@ if __name__ == "__main__":
             speak('thanks for using me')
             print('Quiting...........')
             break
+
         elif 'how are you' in query:
             speak('i am fine! what about you, sir')
-        
-        elif 'school site' in query:
-            webbrowser.open('your school site')
-        
-        elif 'old songs' in query:
-            speak('opening')
-            print('opening...')
-            webbrowser.open('https://www.youtube.com/results?search_query=old+songs')
             
         elif 'introduce yourself' in query:
             speak("I am KLAW. A very high profiled ai voice assistant developed by Cisco a k a manas kishore")
@@ -133,6 +92,5 @@ if __name__ == "__main__":
             s = takeCommand()
             speak(s)
 
-        
-
-        
+        elif 'ecosystem' in query:
+            speak('a biological community of interacting organisms and their physical environment')        
